8cd480129c1321de036ef2c88e308ebc0377aa1c
Mounlight <mouniramoctar2000@gmail.com>
2023-02-21 15:24:44 +0000

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - write a function that prints the alphabet, in lowercase
+*/
+void print_alphabet_x10(void)
+{
+int i, j;
+for (j = 0; j < 10; j++)
+{
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar(10);
+}
+}
