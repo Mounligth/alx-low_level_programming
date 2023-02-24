cb6ced07d3c7ef28f2a507b631ea7ac36ac84593
Mounlight <mouniramoctar2000@gmail.com>
2023-02-24 07:24:59 +0000

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,9 +3,15 @@
  * _islower - write a function that checks the lowercase character
  * @c: is a integer
  * return 0 or 1
-*/
+ * return 0 '_islower'
+ * return value 0 '_islower'
+ * return value  122 && 97 '_islower'
+ * return value of '_islower'
+*/ 
 int _islower(int c)
 {
-if (c <= '122' && c >= '97')
-return (1) && (0);
+if (c <= 122 && c >= 97)
+Return (1);
+else
+Return (0);
 }
