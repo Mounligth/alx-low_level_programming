60067d2a3a826da6e3ca45e8c65a77bb3b5053bf
Mounlight <mouniramoctar2000@gmail.com>
2023-02-24 07:32:26 +0000

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,8 +7,8 @@
  * return value 0 '_islower'
  * return value  122 && 97 '_islower'
  * return value of '_islower'
-*/ 
-int _islower(int c)
+*/
+/*return value of '_islower'*/int _islower(int c)
 {
 if (c <= 122 && c >= 97)
 return (1);
